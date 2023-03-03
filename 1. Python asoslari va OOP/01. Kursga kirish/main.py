9160e89940da40fd5c951a8023244be57bad7e97
Ozodbek Sobirjonovich | Software Developer <ozodbeksobirjonovich@gmail.com>
2023-03-03 05:07:16 +0500

    yangilandi!

---
+++ 1. Python asoslari va OOP/01. Kursga kirish/main.py
@@ -0,0 +1,74 @@
+## DARS REJASI ##
+
+# 1. Python nima?
+# 2. Pythonni o'rnatish.
+# 3. Python interpretatori.
+# 4. Kod muharriri.
+# 5. Birinchi dasturimiz [HelloWorld, Calculator]
+# 6. Python kengaytmalari.
+# 7. Python ilovalari.
+
+###############################################################################################
+
+#### - 1.Python nima? - ####
+
+# Python — o'rganish uchun oson, foydalanish uchun qulay, ko'p qirrali dasturlash tili bo'lib, dasturlashga yangi kirganlar uchun ham, soha mutaxassislari uchun ham zo'r tanlov. Python 1991-yilda gollandiyalik olim Gvido van Rossum tomonidan yaratilgan. Hozirgi versiyasi: Python 3. Ushbu kurs davomida siz bilan Pythonning 3-versiyasidan foydalanamiz. Pythonning ushbu versiyasi birinchi marta 2008-yilda ishlab chiqilgan.
+
+# Dasturlash tili - bu har xil turdagi mashina kodlarini chiqaradigan satrlar to'plamini o'z ichiga olgan rasmiy til. Dasturlash tillari kompyuter tilining bir turi bo'lib, algoritmlarni amalga oshirish uchun kompyuter dasturlashda qo'llaniladi.
+
+#### - 2.Pythonni o'rnatish. - ####
+
+# 1. https://www.python.org saytiga tashrif buyuriladi.
+# 2. Python interpretatori yuklab olinadi. (Download Python).
+# 3. Kompyuterga o'rnatib olinadi! (Install).
+
+#### - 3.Python interpretatori. - ####
+
+# Compiler - Dasturlash tilida yozilgan matnni mashina kodlari to'plamiga o'tkazuvchi dastur.
+
+# Interpreter - Tarjimani bajaradigan dastur. Interpretatsiya kompilyatsiyadan farqli o'laroq, dastur yoki so'rovning dastlabki kodini satr bo'yicha tahlil qilish, qayta ishlash va bajarish, bunda dasturning butun matni uni bajarmasdan ishga tushirishdan oldin tahlil qilinadi va mashina yoki bayt kodiga tarjima qilinadi.
+
+#### - 4.Kod muharriri. - ####
+
+# IDE (Integrated Development) - Dasturlash uchun barcha qo'shimcha kengaytmalari bilan keladigan kod muharriri.
+
+# Mashhur kod muharrirlari: PyCharm Community, Visual Studio Code, Spyder.
+
+#### - 5.Birinchi dasturimiz [HelloWorld, Calculator] - #### 
+
+# 1. Hello World.
+# print("Hello World!")
+
+# 2. Calculator.
+# a = int(input("First number: "))
+# b = int(input("Second number: "))
+# c = a + b
+# d = a - b
+# e = a * b
+# f = a / b
+# print(c, d, e, f, sep=" => ")
+
+#### - 6.Python kengaytmalari. - ####
+
+# Linting - bu potentsial xatolar uchun kodni tahlil qiladigan dasturni ishga tushirish jarayoni.
+
+# Autocomplete - bu dastur foydalanuvchi kiritadigan kodning qolgan qismini bashorat qiladigan xususiyatdir.
+
+# Unit Testing - bu kodning kichik bir qismining ishlashini tekshiradigan dastur. Ishlab chiquvchilar saytlar va ilovalarni muntazam ravishda yangilab turishadi, xususiyatlar qo'shadilar, kodni o'zgartiradilar va tahrir qiladilar, so'ngra hamma narsa qanday ishlashini tekshiradilar.
+
+# Debugging - kompyuter dasturlari, dasturiy ta'minot yoki tizimlarda xatolarni (nuqsonlarni yoki to'g'ri ishlashiga to'sqinlik qiladigan muammolarni) topish va tuzatish jarayoni.
+
+# Formatting - yozilgan kodlarni tushunish uchun oson holatga keltirish holati.
+
+# Code Snippet - kerakli vaqtda kerakli kontekstda qayta-qayta ishlatilishi mumkin bo'lgan kod shabloni.
+
+#### - 7.Python dasturlari. - ####
+
+# Youtube sayti.
+# Google qidiruv tizimi.
+# Netflix sayti.
+# Pinterest tizimi.
+# Instagram.
+# Spotify.
+
+###############################################################################################
